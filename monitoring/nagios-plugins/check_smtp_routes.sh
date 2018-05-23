a926d66b065c9c1f942173419f805cc348ac4d9a
Jan Kucera <jankucera@miniteserakt.local>
2018-05-23 17:04:30 +0200

    smtp exclude

--- monitoring/nagios-plugins/check_smtp_routes.sh
+++ monitoring/nagios-plugins/check_smtp_routes.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+#Takes exclude file as only argiment
+
+if [ "$1" == "" ]; then
+	echo "No exclude file."
+	exit 2
+fi
+
 exitcode=0
 
-for i in $(cat /etc/exim/routelist |awk {'print $2'} | sort | uniq); do
+for i in $(cat /etc/exim/routelist |awk \{'print $2'\} | sort | uniq); do
+	if grep -q -i "$i" "$1"; then
+		continue
+	fi
 	retval=$(/usr/lib64/nagios/plugins/check_tcp -H "$i" -p 25 -t1)
 	retcode=$?
 	if [ $retcode -ne 0 ]; then
