3ac8df53dbd4bddb031fb8a098b25d072e952237
Vladimir Gulas <vladimir.gulas@wygroup.io>
2022-06-16 14:29:28 +0200

    usage

--- monitoring/nagios-plugins/zfs_usage.py
+++ monitoring/nagios-plugins/zfs_usage.py
@@ -9,19 +9,19 @@ CRITICAL = 2
 UNKNOWN = 3
 
 def main():
-    p = Popen(
-      ['zfs', 'list', '-Hpo', 'space'],
-      stdout=PIPE,
-      stderr=PIPE
-    )
-    data_out = []
-    print(f'Usage: | ', end="")
-    for item in p.stdout.readlines():
-        data_out = item.split()
-        NAME = (data_out[0]).decode()
-        USEDSNAP = data_out[3].decode("utf-8")
-        USEDDS = data_out[4].decode("utf-8")
-        print(f'{NAME}={USEDDS};;; {NAME}/snap={USEDSNAP};;;', end=" ")
+  p = Popen(
+    ['zfs', 'list', '-Hpo', 'space'],
+    stdout=PIPE,
+    stderr=PIPE
+  )
+  data_out = []
+  print(f'Usage: | ', end="")
+  for item in p.stdout.readlines():
+      data_out = item.split()
+      NAME = (data_out[0]).decode()
+      USEDSNAP = data_out[3].decode("utf-8")
+      USEDDS = data_out[4].decode("utf-8")
+      print(f'{NAME}={USEDDS};;; {NAME}/snap={USEDSNAP};;;', end=" ")
 
 if __name__ == '__main__':
   try:
