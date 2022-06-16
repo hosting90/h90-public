776d1d571916eee2cf55515a6143b62bff07418e
Vladimir Gulas <vladimir.gulas@wygroup.io>
2022-06-16 13:29:36 +0200

    zfs_usage

---
+++ monitoring/nagios-plugins/zfs_usage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+from subprocess import Popen, PIPE
+
+OK = 0
+WARNING = 1
+CRITICAL = 2
+UNKNOWN = 3
+
+def main():
+  p = Popen(
+    ['zfs', 'list', '-Hpo', 'space'],
+    stdout=PIPE,
+    stderr=PIPE
+  )
+  data_out = []
+  for item in p.stdout.readlines():
+      data_out = item.split()
+      NAME = (data_out[0]).decode("utf-8")
+      USEDSNAP = data_out[3].decode("utf-8")
+      USEDDS = data_out[4].decode("utf-8")
+      print(f'{NAME} | USEDSNAP={USEDSNAP};;; USEDDS={USEDDS};;;')
+
+if __name__ == '__main__':
+  try:
+    main()
+    sys.exit(OK)
+  except:
+    sys.exit(UNKNOWN)
