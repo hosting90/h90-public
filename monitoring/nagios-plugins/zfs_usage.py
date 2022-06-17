b677e4ac43bc621a8876c7f6774c8bd0897bc5b3
Vladimir Gulas <vladimir.gulas@wygroup.io>
2022-06-17 09:49:20 +0200

    decorate ()

--- monitoring/nagios-plugins/zfs_usage.py
+++ monitoring/nagios-plugins/zfs_usage.py
@@ -19,8 +19,8 @@ def main():
   for item in p.stdout.readlines():
       data_out = item.split()
       NAME = (data_out[0]).decode("utf-8")
-      USEDSNAP = data_out[3].decode("utf-8")
-      USEDDS = data_out[4].decode("utf-8")
+      USEDSNAP = (data_out[3]).decode("utf-8")
+      USEDDS = (data_out[4]).decode("utf-8")
       print(f'{NAME}={USEDDS}B;;; {NAME}/snap={USEDSNAP}B;;;', end=" ")
 
 if __name__ == '__main__':
