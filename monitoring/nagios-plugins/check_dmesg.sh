f4ed7393e7dc5c570544dadadb1e264793b57b66
Lukáš Kasič <lukas.kasic@webglobe.com>
2022-07-21 10:09:12 +0200

    nagios-plugins/check_dmesg - search for OOM

--- monitoring/nagios-plugins/check_dmesg.sh
+++ monitoring/nagios-plugins/check_dmesg.sh
@@ -5,7 +5,7 @@
 #
 
 CFGFILE='check_dmesg.cfg'
-REGEX='Hardware Error|I/O error|hard resetting link|DRDY ERR|temperature above threshold|segfault|MEMORY ERROR|dropping packet|This should not happen!! Data will be lost'
+REGEX='Hardware Error|I/O error|hard resetting link|DRDY ERR|temperature above threshold|segfault|MEMORY ERROR|dropping packet|This should not happen!! Data will be lost|Out of memory'
 
 [[ -f "$(dirname $0)/$CFGFILE" ]] && source "$(dirname $0)/$CFGFILE"
 
