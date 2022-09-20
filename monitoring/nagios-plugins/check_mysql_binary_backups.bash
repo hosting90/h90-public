b8450b460ef992df32e3446f8c3e90422ca5fd0a
Tomáš Henzl <tomas.henzl@webglobe.com>
2022-09-20 09:26:42 +0200

    check_mysql_binary_backups.bash -- fix: running backup no longer triggers error

--- monitoring/nagios-plugins/check_mysql_binary_backups.bash
+++ monitoring/nagios-plugins/check_mysql_binary_backups.bash
@@ -5,6 +5,7 @@
 #
 # changelog:
 #   2022/09/16 - created
+#   2022/09/20 - fix: running backup no longer triggers error
 #
 
 ITALIC=$'\e[3m'
@@ -23,7 +24,7 @@ Example usage:
 
 function die() { echo "$1"; exit "${2:-1}"; }
 
-#pidof -xq "$(basename "$0")" && running=1		# we'll use that later
+pidof -xq mariadb-backup xtrabackup && running=1		# we'll use that later
 
 if [[ "$1" == '-h' || "$1" == '--help' ]]; then
 	die "$USAGE" 0
@@ -37,24 +38,28 @@ fi
 
 for item in "${to_check[@]}"; do
 	IFS=: read path age <<< "$item"
-	backups=$(find "$path"/* -mindepth 1 -maxdepth 1 -type f -name 'xtrabackup_info*' -mmin -"$(( age * 60 ))")
+	backups=$(find "$path"/* -mindepth 1 -maxdepth 1 -type f -name 'xtrabackup_info*' -mmin -"$(( age * 60 ))" 2> /dev/null)
 
 	# check backup dirs
 	if [[ -z "$backups" ]]; then
 		msg_preffix='ERROR:'
 		msg_fail+="No backup younger $age hours found in: $path; "
+		[[ "$running" ]] && msg_fail+="Backup is now running; "
 		ret='2'
 	else
 		msg_ok+="$path contains backup(s) younger $age hours; "
 	fi
 
 	# check backup logs
-	log=$(find "$path" -mindepth 1 -maxdepth 1 -type f -name '*.log' -mmin -$(( age * 60 )) -printf "%T@\t%p\n" | sort -nr | head -n1 | cut -f2-)
+	log=$(find "$path" -mindepth 1 -maxdepth 1 -type f -name '*.log' -mmin -$(( age * 60 )) -printf "%T@\t%p\n" 2> /dev/null | sort -nr | head -n1 | cut -f2-)
 
 	if [[ -z "$log" ]]; then
 		msg_preffix='ERROR:'
 		msg_fail+="No backup log younger $age hours found in $path; "
+		[[ "$running" ]] && msg_fail+="Backup is now running; "
 		ret='2'
+	elif [[ "$running" ]]; then
+		msg_ok+="Backup is now running; "
 	elif ! grep -Fq 'completed OK!' "$log"; then
 		msg_preffix='ERROR:'
 		msg_fail+="Last backup log does not contain OK"
