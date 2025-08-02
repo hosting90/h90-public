3c6c5fcfaaaab224a9c4233a143eead6d674a1e9
Jan Kucera <jan.kucera@webglobe.com>
2025-08-02 22:23:10 +0200

    mariadb versions

--- monitoring/nagios-plugins/check_mysql.py
+++ monitoring/nagios-plugins/check_mysql.py
@@ -17,9 +17,10 @@ def main(args):
   try:
     if sys.argv[1] == 'mariadb':
       p = Popen(
-        ['/usr/bin/sudo', 'docker', 'exec', '-u', 'root', '-i', 'c-' + sys.argv[1], 'mariadb-admin', 'ping'],
+        '/usr/bin/sudo docker exec -u root -i c-' + sys.argv[1] + ' /bin/bash -c "if which mariadb-admin; then mariadb-admin ping; else mysqladmin ping; fi"',
         stdout=PIPE,
-        stderr=PIPE
+        stderr=PIPE,
+        shell=True
       )
     else:
       p = Popen(
