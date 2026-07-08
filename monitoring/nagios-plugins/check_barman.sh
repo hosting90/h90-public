50a1c9e5c9d7a835edfa0a57d838b6f8052dba89
Filip Langer <filip.langer@group.one>
2026-07-08 14:32:45 +0200

    feat: Some new feautures - read CHANGELOG inside script.

--- monitoring/nagios-plugins/check_barman.sh
+++ monitoring/nagios-plugins/check_barman.sh
@@ -5,11 +5,16 @@
 #   Contact: filip.langer@group.one
 
 #   CHANGELOG:
+#       08.07.2026 - Updated list-backups (now checking separately full and inc backups)
+#               - New check for error logs (backuping error logs)
+#               - New check for creating graphs of backup folders (disk-space)
 #       07.07.2026 - First version
 
 #   variables
 cluster_hostname="$(hostname | sed -E 's/-[0-9]+(\.)/-01\1/')";
 tmp_file="/tmp/check_barman_detailed_${1}.tmp_${cluster_hostname}";  #  $1 used for specified check
+full_log="/var/log/barman/full_backup.log";
+inc_log="/var/log/barman/incremental_backup.log";
 output="BARMAN backup ${1}";
 
 #   functions
@@ -40,12 +45,30 @@ function read_values() {
 
     local cmd="${1}";
 
-    barman ${cmd} ${cluster_hostname} > ${tmp_file};
-#    if [[ $? -gt 0 ]];
-#    then
-#        error "Error while running [barman ${cmd} ${cluster_hostname}]!";
-#        exit 1;
-#    fi;
+    case "${cmd}" in
+        "logs")
+            tail -n 5 ${full_log} > ${tmp_file}_full;
+            if [[ $? -gt 0 ]];
+            then
+                error "Error while read full log [${full_log}]!";
+            fi;
+
+            tail -n 5 ${inc_log} > ${tmp_file}_inc;
+            if [[ $? -gt 0 ]];
+            then
+                error "Error while read inc log [${inc_log}]!";
+            fi;
+        ;;
+
+        "space")
+            local backup_folder=$(sudo -n -u barman barman show-server ${cluster_hostname} | grep "backup_directory" | awk -F ": " '{print $2}');
+            sudo -n -u barman du -b --max-depth=1 "${backup_folder}/" | awk '{printf "%.2f GiB\t%s\n", $1/1024/1024/1024, $2}' > ${tmp_file};  # in GiB
+        ;;
+
+        *)
+            sudo -n -u barman barman ${cmd} ${cluster_hostname} > ${tmp_file};
+        ;;
+    esac;
 }
 
 #   script body
@@ -101,17 +124,18 @@ case ${1} in
     ;;
 
     "list-backups")
-        info_text="${1} ${cluster_hostname} (backups)";
+        info_text="${1} ${cluster_hostname} (full/inc)";
         result="";
         end_code=0;
 
         read_values "${1}";
-        counter_backups=$(cat ${tmp_file} | wc -l);
+        counter_full_backups=$(cat ${tmp_file} | grep "\- F \-" | wc -l);
+        counter_inc_backups=$(cat ${tmp_file} | grep "\- I \-" | wc -l);
 
-        info_text="${info_text} (${counter_backups})";
-        result="number_of_backups=${counter_backups};0;0;0;";
+        info_text="${info_text} (${counter_full_backups}/${counter_inc_backups})";
+        result="number_of_full_backups=${counter_full_backups};0;0;0; number_of_inc_backups=${counter_inc_backups};0;0;0;";
 
-        if [[ "${counter_backups}" -eq 0 ]];
+        if [[ "${counter_full_backups}" -eq 0 ]] || [[ "${counter_inc_backups}" -eq 0 ]];
         then
             end_code=1;
         fi;
@@ -123,6 +147,53 @@ case ${1} in
         else
             output="${output} PROBLEM - NO BACKUPS AVAILABLE: ${info_text} | ${result}";        
         fi;    
+    ;;
+
+    "logs")
+        info_text="${1} ${cluster_hostname} (full_err/inc_err)";
+        result="";
+        end_code=0;
+
+        read_values "${1}";
+        counter_full_errors=$(cat ${tmp_file}_full | grep "ERROR" | wc -l);
+        counter_inc_errors=$(cat ${tmp_file}_inc | grep "ERROR" | wc -l);
+
+        info_text="${info_text} (${counter_full_errors}/${counter_inc_errors})";
+        result="number_of_full_errors=${counter_full_errors};0;1;1; number_of_inc_errors=${counter_inc_errors};0;1;1;";
+
+        if [[ "${counter_full_backups}" -gt 0 ]] || [[ "${counter_inc_backups}" -gt 0 ]];
+        then
+            end_code=1;
+        fi;
+
+        #   return info
+        if [[ ${end_code} -eq 0 ]];
+        then
+            output="${output} OK: ${info_text} | ${result}";
+        else
+            output="${output} PROBLEM - BACKUPING ERRORS FOUND: ${info_text} | ${result}";        
+        fi;        
+    ;;
+
+    "space")
+        info_text="${1} ${cluster_hostname} [GiB] (Basebackups/WALs)";
+        result="";
+        end_code=0;
+
+        read_values "${1}";
+        base_gb=$(cat ${tmp_file} | grep "/base" | awk '{print $1}');
+        wal_gb=$(cat ${tmp_file} | grep "/wals" | awk '{print $1}');
+
+        info_text="${info_text} (${base_gb}/${wal_gb})";
+        result="base_backups_gb=${base_gb};0;;; wal_gb=${wal_gb};0;;;";
+
+        #   return info
+        if [[ ${end_code} -eq 0 ]];
+        then
+            output="${output} OK: ${info_text} | ${result}";
+        else
+            output="${output} PROBLEM - NOT ENOUGH SPACE: ${info_text} | ${result}";        
+        fi;         
     ;;
 esac;
 
